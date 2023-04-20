30a1a2ef9496c07cbca2191e1aad534bb25dcb46
Sergio Reinoso <slrs993@gmail.com>
2023-04-20 17:54:44 -0500

    Crear cartas en el HTML con JS

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,14 @@ let deck            = [];
 const tipos         = ['C', 'D', 'H', 'S'];
 const especiales    = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0, puntosComputadora = 0;
+
+//Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir');
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const puntosHTML = document.querySelectorAll('small');
+
 //Esta funcion crea una bueva baraja
 const crearDeck = () => {
 
@@ -43,9 +51,6 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop();
-
-    console.log(deck);
-    console.log(carta);
     return carta;
 
 }
@@ -60,5 +65,26 @@ const valorCarta = (carta) => {
 
 }
 
-const valor = valorCarta(pedirCarta());
-console.log({valor});
+//Eventos
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    //<!-- <img class="carta" src="./assets/cartas/2C.png" alt=""> -->
+    const imgCarta = document.createElement('img');
+    imgCarta.src= `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if(puntosJugador > 21){
+        console.warn('Lo siento mucho, ya perdio');
+        btnPedir.disabled = true;
+    } else if (puntosJugador === 21){
+        console.warn('21, genial');
+        btnPedir.disabled = true;
+    }
+
+});

ca20181e6c5323edfcebce649ef0c420ed6824cc
Sergio Reinoso <slrs993@gmail.com>
2023-04-23 21:38:01 -0500

    Turno de la computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -13,8 +13,10 @@ let puntosJugador = 0, puntosComputadora = 0;
 
 //Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
 
 const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 //Esta funcion crea una bueva baraja
@@ -65,6 +67,31 @@ const valorCarta = (carta) => {
 
 }
 
+//Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+
+    do {
+
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        puntosHTML[1].innerText = puntosComputadora;
+
+        //<!-- <img class="carta" src="./assets/cartas/2C.png" alt=""> -->
+        const imgCarta = document.createElement('img');
+        imgCarta.src= `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append(imgCarta);
+
+        if(puntosMinimos > 21){
+            break;
+        }
+
+    } while((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+
+}
+
 //Eventos
 btnPedir.addEventListener('click', () => {
 
@@ -82,9 +109,22 @@ btnPedir.addEventListener('click', () => {
     if(puntosJugador > 21){
         console.warn('Lo siento mucho, ya perdio');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21){
         console.warn('21, genial');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
+
+});
+
+btnDetener.addEventListener('click', () => {
+
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    turnoComputadora(puntosJugador);
 
 });
